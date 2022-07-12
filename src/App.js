7374c1e4b6ade9d2c6f59f72dffc99e309c3866c
Aleksandra Wierzbiak <alewierzbiak@gmail.com>
2022-07-12 12:52:37 +0200

    feat: Add card component, structure components folders, refactor functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -30,8 +30,6 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>Ooh lalala, I love React baby!</p>
       <Expenses expenses={expenses}/>
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
   return(
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -19,7 +20,7 @@ function Expenses(props) {
         amount={props.expenses[2].amount}
         date={props.expenses[2].date}
       />
-    </div>
+    </Card>
   );
 }
 
