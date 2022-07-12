29090398d3ac52aa906c752183666dcd54f0b4a2
Aleksandra Wierzbiak <alewierzbiak@gmail.com>
2022-07-12 12:18:43 +0200

    Init repo

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div className="expense-item__description">
+        <ExpenseDate date={props.date}/>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
