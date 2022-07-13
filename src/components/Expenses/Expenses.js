6ea0e3bedde48fe80ddb6ef9d0ac4b72025b65bd
Aleksandra Wierzbiak <alewierzbiak@gmail.com>
2022-07-13 09:00:53 +0200

    feat: Add expenses filter and change expense item to stateless component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,26 +1,38 @@
-import ExpenseItem from './ExpenseItem';
+import { useState } from 'react';
 import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [expensesYear, setExpensesYear] = useState('2020');
+
+  const filterExpensesHandler = (year) => {
+    setExpensesYear(year);
+    console.log(expensesYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selectedYear={expensesYear} onFilterByYear={filterExpensesHandler}/>
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+      </Card>
+    </div>
   );
 };
 
